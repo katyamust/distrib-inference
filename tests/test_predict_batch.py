cedb2afdeb4341df436257c1e197dc3bc285ec8e
katyamust <katya.mustafina@gmail.com>
2021-11-25 15:00:29 +0000

    fix unit test predict_batch

--- tests/test_predict_batch.py
+++ tests/test_predict_batch.py
@@ -3,7 +3,7 @@ import pytest
 import numpy as np
 from pyspark.sql.functions import col
 from pyspark.ml.linalg import Vectors
-from distrib_resnet import predict_batch
+from distrib_resnet.predict_batch import predict_batch
 
 
 @pytest.fixture()
@@ -11,19 +11,23 @@ def spark_df(spark):
     """
     Generate small sample dataframe for test
     """
-    df = np.concatenate([np.arange(5),
-                     2*np.arange(5),
-                     3*np.arange(5),
-                     4*np.arange(5),
-                     5*np.arange(5)]).reshape(5,-1)
-    dff = map(lambda x: (int(x[1]), Vectors.dense(x[1:])), df)
+    lengh = 10
+    nrows = 5
+    df = np.concatenate([np.arange(lengh),
+                     2*np.arange(lengh),
+                     3*np.arange(lengh),
+                     4*np.arange(lengh),
+                     5*np.arange(lengh)]).reshape(nrows,-1)
+    dff = map(lambda x: (1,Vectors.dense(x)), df)
+    #dff = map(lambda x: (int(x[1]), Vectors.dense(x[1:])), df)
 
-    mydf = spark.createDataFrame(dff,schema=["label", "features"])
+    mydf = spark.createDataFrame(dff,schema=["_", "features"])
     return mydf
 
 def test_predict_batch(spark_df):
 
+    nrows = spark_df.count()
     predictions_df = spark_df.select(col('features'), predict_batch(col('features')).alias("prediction"))
-    predictions_df.write.mode("noop").parquet("")
+    nprows = predictions_df.count()
 
-    assert True
+    assert nrows == nprows
